2249a9f8a2c43c65b3407eca9371b989cdeceb6f
kvncaldwll <kvn.caldwll@gmail.com>
2015-10-05 15:44:50 -0700

    added comments

--- rearrangewords.py
+++ rearrangewords.py
@@ -3,21 +3,18 @@ import random
 
 if __name__ == "__main__":
     user_input = sys.argv[1:]
+    # for single word input
     if len(user_input) == 1:
         rev = ''.join(user_input)
+        # reverse string
         print(rev[::-1])
+        # randomize string
         rand = ''.join(random.sample(user_input[0], len(user_input[0])))
         print(rand)
+    # for multiple word input
     else:
-        # print reversed
+        # print reversed list / reversed word order
         print(' '.join(user_input[::-1]))
-        # print shuffled
+        # print shuffled list / random word order
         random.shuffle(user_input)
         print(' '.join(user_input))
-import sys
-from random import shuffle
-
-if __name__ == "__main__":
-    user_input = sys.argv[1:]
-    shuffle(user_input)
-    print(user_input)
