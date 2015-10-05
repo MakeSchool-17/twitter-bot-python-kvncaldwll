f58be8856f05587ec7098fa7e5306c3429523961
kvncaldwll <kvn.caldwll@gmail.com>
2015-10-05 16:28:54 -0700

    print dict words with match beginning

--- randomdictionary.py
+++ randomdictionary.py
@@ -1,17 +1,21 @@
-import random
+# import random
 import sys
 
 dict_file = open('/usr/share/dict/words').read().splitlines()
 
 
 if __name__ == "__main__":
-    # turn into integer
-    user_num = int(sys.argv[1])
-    # select amount of words = to integer
-    for i in range(0, user_num):
-        # random index number from dict file
-        rand_words = random.randint(0, len(dict_file) - 1)
-        # print single line
-        sys.stdout.write(dict_file[rand_words] + ' ')
-    # line break
-    sys.stdout.write('\n')
+    # # turn into integer
+    # user_input_num = int(sys.argv[1])
+    # # select amount of words = to integer
+    # for i in range(0, user_num):
+    #     # random index number from dict file
+    #     rand_words = random.randint(0, len(dict_file) - 1)
+    #     # print single line
+    #     sys.stdout.write(dict_file[rand_words] + ' ')
+    # # line break
+    # sys.stdout.write('\n')
+    user_input = sys.argv[1]
+    for word in dict_file:
+        if word[:len(user_input)] == user_input:
+            print(word)
