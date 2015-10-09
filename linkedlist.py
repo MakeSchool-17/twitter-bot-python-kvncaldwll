49becabcdc7279132c7ff84e16bded610626442f
kvncaldwll <kvn.caldwll@gmail.com>
2015-10-09 15:40:23 -0700

    double linked lle link list nodes

--- linkedlist.py
+++ linkedlist.py
@@ -1,54 +1,83 @@
-
 class Node(object):
-
-    def __init__(self, data=None):
+    def __init__(self, data = None):
         self.data = data
         self.next = None
+        self.prev = None
 
+    def __str__(self):
+        # notify if node points to another node
+        has_next = ' , has next' if self.next else ''
+        return 'Node({}{})'.format(self.data, has_next)
 
 class LinkedList(object):
 
     def __init__(self):
-        # empty LinkedList
+        #empty linked list
         self.head = None
         self.tail = None
         self.size = 0
 
-    def insert_at_head(self, data):
-        # create a new node with the data
+    def __str__(self):
+        # notify if node at head and or tail position
+        head_str = 'head: ' + str(self.head) if self.head else ''
+        tail_str = 'tail: ' + str(self.tail) if self.head else ''
+        return 'LinkedList({}{})'.format(head_str, tail_str)
+
+    def is_empty(self):
+        return self.size == 0
+
+    def is_valid(self):
+        # check if node is in LinkedList
+        head_true = self.head is not None
+        tail_true = self.tail  is not None
+        size_true = self.size > 0
+        if head_true and tail_true and size_true:
+            return True
+        elif not head_true and not tail_true and not size_true:
+            return True
+        else:
+            return False
+
+    def insert_head(self, data):
+        # create a node
         new_node = Node(data)
-        new_node.next = self.head
-        # reassign head reference
+        # new node points to previous head node
+        new_node.next  = self.head
+        # old head points to node that comes before it
+        self.head.prev = new_node
+        #change head to new node
         self.head = new_node
+        # update LinkedList size
         self.size += 1
-        # assign tail reference if list is empty
+        # if no tail pointer, means only one node which is both head and tail
         if self.tail is None:
             self.tail = new_node
 
-    def insert_at_tail(self, data):
+    def insert_tail(self, data):
         new_node = Node(data)
+        # if LinkedList is empty set new node as head
         if self.is_empty():
-            # assign head reference
             self.head = new_node
         else:
-            # append new node after tail
+            # append new node to existing tail
             self.tail.next = new_node
-        # reassign tail reference
+            # new tail node points to node that comes before it
+            new_node.prev = self.tail
         self.tail = new_node
         self.size += 1
 
     def remove_head(self):
-        if self.head is None:
-            raise ValueError('List is empty')
+        if self.is_empty():
+            raise ValueError ('linked list is empty!')
         self.head = self.head.next
         self.size -= 1
 
-    # running time: O(n)
-    def calculate_size(self):
-        count = 0
-        current_node = self.head
-        while current_node is not None:
-            count += 1
-            current_node = current_node.next
-        return count
+    def remove_tail(self):
+        if self.is_empty():
+            raise ValueError ('linked list is empty!')
+        else:
+            self.tail = self.tail.prev
+            self.size -= 1
 
+    def calculate_size(self):
+        return str(self.size)
